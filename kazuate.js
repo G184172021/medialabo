94bde516731ddf19bdc636640050f00f42ccfa49
G184172021 <218417@st.takushoku-u.ac.jp>
2022-05-19 15:28:03 +0900

    tuika

--- kazuate.js
+++ kazuate.js
@@ -8,7 +8,8 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 hantei();
-
+let dp = document.querySelector('div#placeholder');
+let d = document.createElement('div');
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
@@ -21,20 +22,25 @@ console.log(kaisu+"回目の予想:"+yoso);
 if (kaisu < 4) {
 if (yoso===kotae) {
     if (kaisu === 1){
-    console.log("正解です。おめでとう！");
+        console.log("正解です。おめでとう！");
+    
 }
 else {
-    console.log("答えは"+kotae+"でした。すでにゲームは終了しています。")
+    console.log("答えは"+kotae+"でした。すでにゲームは終了しています。");
+   
 }
 }
 else if(yoso<kotae) {
     console.log("まちがい。答えはもっと小さいですよ");
+
 }
 else{
-        console.log("まちがい。答えはもっと大きいですよ");
+    console.log("まちがい。答えはもっと大きいですよ");
+
     }
 }
 else {
     console.log("答えは"+kotae+"でした。すでにゲームは終了しています。")
 }
+dp.insertAdjacentElement('beforeend', d);
 }
